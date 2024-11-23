e407e6081afc2629c5a8303a80d26d048855d508
sensor-dream <sensor-dream@sensor-dream.ru>
2024-11-23 20:36:46 +0700

    commit

--- tools/chmodx.bash
+++ tools/chmodx.bash
@@ -1,13 +1,30 @@
 #!/bin/env bash
 
+if [[ -z $@ ]]; then
+    path="$(dirname $(readlink -m $0))";
+else
+    path="${@%/}";
+fi
+
+echo "Find fale in ${path}";
+
+if [[ ! -d "${path}" ]]; then
+    echo "Not found path: ${path}";
+    exit 1;
+fi
+
 multiple_cmd() {
-  if grep -aiHsrE -m 1 '^#!(/usr/|/)[sb]+in(/env[[:space:]]+|/)(bash|sh|py[thon]+|js|perl|php)$' "${@}"; then
-    sudo chmod +x "${@}"
-  else
-    sudo chmod -x "${@}"
-  fi
+    if [[ -x "${@}" ]]; then
+	if grep -aiHsrE -m 1 '^#!(/usr/|/)[sb]+in(/env[[:space:]]+|/)(bash|sh|py[thon]+|js|perl|php)$' "${@}"; then
+	    sudo chmod +x "${@}"
+            echo "+x ${@}";
+        else
+            sudo chmod -x "${@}"
+            echo "-x ${@}";
+        fi
+    fi
 }
 
 export -f multiple_cmd
 
-find . -type f -exec bash -c 'multiple_cmd "$0"' {} \;
+find "${path}" -type f -exec bash -c 'multiple_cmd "$0"' {} \;
